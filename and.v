f38a2d15ec67b37f82c8c6cd1be175079333591d
Nathan <136270833+nabrandman@users.noreply.github.com>
2023-11-29 09:56:09 -0600

    Delete and.v

--- and.v
+++
@@ -1,5 +0,0 @@
-module and1 (input a, input b, output y);
-    //i know this might seem unnecessary and silly, but I want the alu itself to be consistent in referencing other modules for each different thing it can do
-    and(y, a,b);
-
-endmodule
